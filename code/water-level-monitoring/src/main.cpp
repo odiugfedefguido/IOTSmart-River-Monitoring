1e41cdf56317777dec4b0d96fd19e8f2c6eb6392
ilmagodiodds <chiara.denardi2@studio.unibo.it>
2024-01-22 13:04:58 +0100

    esp sistemato

--- code/water-level-monitoring/src/main.cpp
+++ code/water-level-monitoring/src/main.cpp
@@ -1,18 +1,21 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int ledPin1 = 27;  // Pin del primo LED
+const int ledPin2 = 25;  // Pin del secondo LED
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(ledPin1, OUTPUT);  // Imposta il pin del primo LED come output
+  pinMode(ledPin2, OUTPUT);  // Imposta il pin del secondo LED come output
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  // Accende il primo LED e spegne il secondo
+  digitalWrite(ledPin1, HIGH);
+  digitalWrite(ledPin2, LOW);
+  delay(1000);  // Attendi 1 secondo
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // Accende il secondo LED e spegne il primo
+  digitalWrite(ledPin1, LOW);
+  digitalWrite(ledPin2, HIGH);
+  delay(1000);  // Attendi 1 secondo
 }
